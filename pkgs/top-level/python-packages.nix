43e7db1d7e173c89528d5a0faaa69597ff0f636c
Robert Schütz <nix@dotlambda.de>
2025-05-06 10:11:38 -0700

    python313Packages.pyephember: drop

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12248,8 +12248,6 @@ self: super: with self; {
 
   pyenvisalink = callPackage ../development/python-modules/pyenvisalink { };
 
-  pyephember = callPackage ../development/python-modules/pyephember { };
-
   pyephember2 = callPackage ../development/python-modules/pyephember2 { };
 
   pyepsg = callPackage ../development/python-modules/pyepsg { };
