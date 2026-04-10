c38f12bf3104666f5aa0f8fa05b7208bef51f5f6
Josef Hofer <me@jhofer.de>
2026-04-10 17:09:25 +0200

    moonlight-qt: 6.1.0 -> 6.1.0-unstable-2026-04-10
    Changelog:
    https://github.com/moonlight-stream/moonlight-qt/commits/master

--- pkgs/by-name/mo/moonlight-qt/package.nix
+++ pkgs/by-name/mo/moonlight-qt/package.nix
@@ -2,7 +2,6 @@
   stdenv,
   lib,
   fetchFromGitHub,
-  fetchpatch,
   qt6,
   pkg-config,
   vulkan-headers,
@@ -19,28 +18,20 @@
   libxkbcommon,
   wayland,
   libdrm,
+  apple-sdk_15 ? null,
   nix-update-script,
 }:
-
 stdenv.mkDerivation (finalAttrs: {
   pname = "moonlight-qt";
-  version = "6.1.0";
+  version = "6.1.0-unstable-2026-04-10";
 
   src = fetchFromGitHub {
     owner = "moonlight-stream";
     repo = "moonlight-qt";
-    tag = "v${finalAttrs.version}";
-    hash = "sha256-rWVNpfRDLrWsqELPFquA6rW6/AfWV+6DNLUCPqIhle0=";
+    rev = "af03f57e08a2c183ecd09b93f3a59964d473ffe6";
+    hash = "sha256-CVtYb9TyRaSYmoO5+2YXSGv6KdntTsaYnz7RrOcwyKE=";
     fetchSubmodules = true;
   };
-
-  patches = [
-    # Fix build for Xcode < 14
-    (fetchpatch {
-      url = "https://github.com/moonlight-stream/moonlight-qt/commit/76deafbd7bf868562d69061e7d6abf2612a2c7ad.patch";
-      hash = "sha256-+rXdexZQpOP6yS+oTmvYVxasWxOX16uU1udN75zNX3w=";
-    })
-  ];
 
   nativeBuildInputs = [
     qt6.qmake
@@ -68,6 +59,9 @@ stdenv.mkDerivation (finalAttrs: {
     qt6.qtwayland
     wayland
     libdrm
+  ]
+  ++ lib.optionals (stdenv.hostPlatform.isDarwin && apple-sdk_15 != null) [
+    apple-sdk_15
   ];
 
   qmakeFlags = [ "CONFIG+=disable-prebuilts" ];
@@ -78,10 +72,16 @@ stdenv.mkDerivation (finalAttrs: {
     ln -s $out/Applications/Moonlight.app/Contents/MacOS/Moonlight $out/bin/moonlight
   '';
 
-  passthru.updateScript = nix-update-script { };
+  # nix-update moonlight-qt --version branch=master --build --commit
+  passthru.updateScript = nix-update-script {
+    extraArgs = [
+      "--version"
+      "branch=master"
+    ];
+  };
 
   meta = {
-    changelog = "https://github.com/moonlight-stream/moonlight-qt/releases/tag/v${finalAttrs.version}";
+    changelog = "https://github.com/moonlight-stream/moonlight-qt/commits/master";
     description = "Play your PC games on almost any device";
     homepage = "https://moonlight-stream.org";
     license = lib.licenses.gpl3Plus;
